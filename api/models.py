bb2dd065b832fa0488b959267f733258a553ad90
fat83dotcom <fernando.quimic@gmail.com>
2025-01-23 17:51:51 -0300

    Modificando nome do campo.

--- api/models.py
+++ api/models.py
@@ -5,7 +5,7 @@ class Customer(models.Model):
     name = models.CharField(max_length=128)
     email = models.EmailField()
     phone = models.CharField(max_length=20)
-    adress = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
 
 
 class Product(models.Model):
