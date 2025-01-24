4c80d31b97aff94392c7b6ddc4e0ebdbbe529235
fat83dotcom <fernando.quimic@gmail.com>
2025-01-24 12:13:23 -0300

    Definindo as novas rotas.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from api.views import CustomerGetView, CustomerPostView
+from api.views import CustomerGetView, CustomerPostView, ProductPostView
 
 urlpatterns = [
     path('get_customers/', CustomerGetView.as_view(), name='customers_g'),
-    path('post_customers/', CustomerPostView.as_view(), name='customers_p'),
+    path('register_customers/', CustomerPostView.as_view(), name='customers_p'),
+    path('register_product/', ProductPostView.as_view(), name='customers_p'),
 ]
