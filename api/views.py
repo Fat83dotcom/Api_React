c3a821f244296873896e48540024a0d450e93a8d
fat83dotcom <fernando.quimic@gmail.com>
2025-01-24 12:14:14 -0300

    Novas views para os produtos

--- api/views.py
+++ api/views.py
@@ -2,28 +2,26 @@
 from rest_framework.views import APIView
 from rest_framework import serializers
 from rest_framework.response import Response
-from api.models import Customer
+from api.models import Customer, Product
 from rest_framework import status
 
 
-class CustomerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Customer
-        fields = [
-            'name',
-            'email',
-            'phone',
-            'address',
-        ]
-    
-    def create(self, validated_data):
-        return Customer.objects.create(**validated_data)
 
 
 class CustomerGetView(APIView):
     def get(self, request):
         query = Customer.objects.all()
         serializer = CustomerSerializer(
+            instance=query,
+            many=True
+        )
+        return Response(serializer.data)
+
+
+class ProductGetView(APIView):
+    def get(self, request):
+        query = Product.objects.all()
+        serializer = ProductSerializer(
             instance=query,
             many=True
         )
@@ -37,3 +35,40 @@ class CustomerPostView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ProductPostView(APIView):
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = [
+            'name',
+            'second_name',
+            'email',
+            'phone',
+            'address',
+        ]
+    
+    def create(self, validated_data):
+        return Customer.objects.create(**validated_data)
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            'name',
+            'price',
+            'quantity'
+        ]
+
+    def create(self, validated_data):
+        return Product.objects.create(**validated_data)
