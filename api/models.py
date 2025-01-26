d264f2148178df17687f2a84669551c527e5340b
fat83dotcom <fernando.quimic@gmail.com>
2025-01-25 22:56:05 -0300

    criando o model productcategory  e adicionando a chave estrangeira ao produto.

--- api/models.py
+++ api/models.py
@@ -9,16 +9,26 @@ class Customer(models.Model):
     address = models.CharField(max_length=255)
 
 
+class ProductCategory(models.Model):
+    category_name = models.CharField(max_length=100)
+
+
 class Product(models.Model):
     name = models.CharField(max_length=128)
     price = models.FloatField()
     quantity = models.IntegerField()
+    category = models.ForeignKey(
+        to=ProductCategory, default='Sem Categoria', on_delete=models.SET_DEFAULT
+    )
 
 
 class Order(models.Model):
-    id_customer = models.ForeignKey(to=Customer, related_name='customer', on_delete=models.CASCADE)
+    id_customer = models.ForeignKey(
+        to=Customer, related_name='customer', on_delete=models.CASCADE
+    )
     date = models.DateField()
     total = models.FloatField()
+    order_status = models.BooleanField(default=True)
 
 
 class OrderItems(models.Model):
