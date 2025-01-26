6170839bf6704682be132755e1374dbe51730bbb
fat83dotcom <fernando.quimic@gmail.com>
2025-01-25 23:56:54 -0300

    Definindo nome desses objetos.

--- api/models.py
+++ api/models.py
@@ -8,9 +8,15 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f'{self.category_name}'
+
 
 class ProductCategory(models.Model):
     category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.category_name}'
 
 
 class Product(models.Model):
@@ -18,8 +24,11 @@ class Product(models.Model):
     price = models.FloatField()
     quantity = models.IntegerField()
     category = models.ForeignKey(
-        to=ProductCategory, default='Sem Categoria', on_delete=models.SET_DEFAULT
+        to=ProductCategory, on_delete=models.DO_NOTHING
     )
+
+    def __str__(self):
+        return f'{self.category_name}'
 
 
 class Order(models.Model):
@@ -30,8 +39,14 @@ class Order(models.Model):
     total = models.FloatField()
     order_status = models.BooleanField(default=True)
 
+    def __str__(self):
+        return f'{self.category_name}'
+
 
 class OrderItems(models.Model):
     id_order = models.ForeignKey(to=Order, related_name='order_items', on_delete=models.CASCADE)
     id_product = models.ForeignKey(to=Product, related_name='product', on_delete=models.CASCADE)
     quantity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.id_order}'
