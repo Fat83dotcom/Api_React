1b06ad27400a696d9fef243188dbd2e7bdfe1f63
fat83dotcom <fernando.quimic@gmail.com>
2025-01-27 11:43:18 -0300

    Rota para pesquisa de clientes.

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from api.views import CustomerGetView, CustomerPostView, ProductPostView
+from api.views import CustomerGetView, CustomerPostView, ProductPostView, CustomerGetSearchView
 
 urlpatterns = [
     path('get_customers/', CustomerGetView.as_view(), name='customers_g'),
     path('register_customers/', CustomerPostView.as_view(), name='customers_p'),
     path('register_product/', ProductPostView.as_view(), name='customers_p'),
+    path('search_customer/', CustomerGetSearchView.as_view(), name='search_customer_g')
 ]
