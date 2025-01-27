1afdbadcff7157fadfeed9a29135e1e3bf8b4226
fat83dotcom <fernando.quimic@gmail.com>
2025-01-27 17:45:29 -0300

    criando urls.

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from api.views import CustomerGetView, CustomerPostView, ProductPostView, CustomerGetSearchView
+from api.views import CategoryGetView
 
 urlpatterns = [
     path('get_customers/', CustomerGetView.as_view(), name='customers_g'),
     path('register_customers/', CustomerPostView.as_view(), name='customers_p'),
     path('register_product/', ProductPostView.as_view(), name='customers_p'),
-    path('search_customer/', CustomerGetSearchView.as_view(), name='search_customer_g')
+    path('search_customer/', CustomerGetSearchView.as_view(), name='search_customer_g'),
+    path('search_product_category/', CategoryGetView.as_view(), name='search_product_category'),
 ]
