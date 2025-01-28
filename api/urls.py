b40a1f7da318f0d11639e9d7ff3a77e5023d407c
fat83dotcom <fernando.quimic@gmail.com>
2025-01-27 21:42:39 -0300

    Rota para gravar as categorias.

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from api.views import CustomerGetView, CustomerPostView, ProductPostView, CustomerGetSearchView
-from api.views import CategoryGetView
+from api.views import CategoryGetView, ProductCategoryPostView
 
 urlpatterns = [
     path('get_customers/', CustomerGetView.as_view(), name='customers_g'),
@@ -8,4 +8,9 @@ urlpatterns = [
     path('register_product/', ProductPostView.as_view(), name='customers_p'),
     path('search_customer/', CustomerGetSearchView.as_view(), name='search_customer_g'),
     path('search_product_category/', CategoryGetView.as_view(), name='search_product_category'),
+    path(
+        'register_product_category/',
+        ProductCategoryPostView.as_view(),
+        name='register_product_category'
+    ),
 ]
