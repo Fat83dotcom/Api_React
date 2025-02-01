71a70515fa0bd4c6cc9ec94ec7b9fcc0aece01c1
fat83dotcom <fernando.quimic@gmail.com>
2025-02-01 19:02:57 -0300

    aplicando flake 8

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from api.views import CustomerGetView, CustomerPostView, ProductPostView, CustomerGetSearchView
-from api.views import CategoryGetView, ProductCategoryPostView, CreateOrder, SearchOrderCustomerIdGetView
-from api.views import ProductGetView, SearchProductByCategory, SearchProductByName, AppendItemsToOrder
+from api.views import CustomerGetView, CustomerPostView
+from api.views import ProductPostView, CustomerGetSearchView
+from api.views import CategoryGetView, ProductCategoryPostView
+from api.views import CreateOrder, SearchOrderCustomerIdGetView
+from api.views import ProductGetView, SearchProductByCategory
+from api.views import SearchProductByName, AppendItemsToOrder
 
 urlpatterns = [
     path('get_products/', ProductGetView.as_view(), name='product_g'),
     path('get_customers/', CustomerGetView.as_view(), name='customers_g'),
-    path('register_customers/', CustomerPostView.as_view(), name='customers_p'),
+    path(
+        'register_customers/', CustomerPostView.as_view(), name='customers_p'
+    ),
     path('register_product/', ProductPostView.as_view(), name='customers_p'),
     path(
         'search_customer/',
